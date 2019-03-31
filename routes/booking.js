f0a396771234e818a21fe456939f6ec7bfe38c68
patrickjiang <jzc1828@gmail.com>
2019-03-30 17:26:14 -0700

    Added search function for flight table; updated styling using Bootstrap

--- routes/booking.js
+++ routes/booking.js
@@ -2,92 +2,83 @@ const express = require('express');
 const router = express.Router()
 const db = require('../config/database');
 const Booking = require('../models/Booking');
-const Sequelize = require ('sequelize');
+const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
-// find all the rows in the booking table 
-router.get('/', (req, res) => 
+/******************************************* Display all rows in the booking table ********************************/
+router.get('/', (req, res) =>
     Booking.findAll()
-        .then(booking => res.render('booking',{
-               booking
-           }))
+        .then(booking => res.render('booking', {
+            booking
+        }))
         .catch(err => console.log(err)));
 
-// // find the row(s) in booking table where the payment method is Credit
-// router.get('/', (req, res) => 
-//     Booking.findAll({where: {paymentMethod: 'Credit'}})
-//         .then(booking => {
-//             res.render('booking', {
-//                 booking
-//             });
-//         })
-//         .catch(err => console.log(err)));
 
-
-// Display add booking form
+/********************************************** Add a new booking *************************************************/
+// Display form to add a new booking
 router.get('/add', (req, res) => res.render('add'));
 
-//add booking
+// Add a booking
 router.post('/add', (req, res) => {
     // destructure the data object
     let { bookingDate, paymentAmount, paymentMethod, flightId, customerId, userId } = req.body;
     let errors = [];
-    
-    if(!bookingDate) {
-        errors.push({ text: 'Please add booking date'})
+
+    if (!bookingDate) {
+        errors.push({ text: 'Please add booking date' })
     }
-    if(!paymentAmount) {
-        errors.push({ text: 'Please add payment amount'})
+    if (!paymentAmount) {
+        errors.push({ text: 'Please add payment amount' })
     }
-    if(!paymentMethod) {
-        errors.push({ text: 'Please add a payment method'})
+    if (!paymentMethod) {
+        errors.push({ text: 'Please add a payment method' })
     }
-    if(!flightId) {
-        errors.push({ text: 'Please add a flight id'})
+    if (!flightId) {
+        errors.push({ text: 'Please add a flight id' })
     }
-    if(!customerId) {
-        errors.push({ text: 'Please add a customer id'})
+    if (!customerId) {
+        errors.push({ text: 'Please add a customer id' })
     }
-    if(!userId) {
-        errors.push({ text: 'Please add a user id'})
+    if (!userId) {
+        errors.push({ text: 'Please add a user id' })
     }
 
     // Check for errors
     if (errors.length > 0) {
         res.render('add', {
             errors,
-            bookingDate, 
-            paymentAmount, 
-            paymentMethod, 
-            flightId, 
-            customerId, 
+            bookingDate,
+            paymentAmount,
+            paymentMethod,
+            flightId,
+            customerId,
             userId
         });
     } else {
-        if(!paymentAmount) {
+        if (!paymentAmount) {
             paymentAmount = 0;
         } else {
             paymentAmount = `${paymentAmount}`;
         }
-        
+
         // Make paymentMethod lowercase and remove space after comma
         paymentMethod = paymentMethod.toLowerCase().replace(/, /g, ',');
-        
+
         // Insert data into the flight table
         Booking.create({
-            bookingDate, 
-            paymentAmount, 
-            paymentMethod, 
-            flightId, 
-            customerId, 
+            bookingDate,
+            paymentAmount,
+            paymentMethod,
+            flightId,
+            customerId,
             userId
         })
             .then(booking => res.redirect('/booking'))
             .catch(err => console.log(err));
-        }
-    });
+    }
+});
 
-// Search for bookings
+/********************************************** Search bookings ****************************************************/
 router.get('/search', (req, res) => {
     // destructure querry object
     let { bookingIdValue } = req.query;
@@ -101,7 +92,7 @@ router.get('/search', (req, res) => {
     paymentMethodValue = paymentMethodValue.toLowerCase();
     paymentAmountValue = paymentAmountValue.toLowerCase();
 
-    Booking.findAll({ 
+    Booking.findAll({
         where: {
             // booking Id = bookingIdValue
             bookingId: {
@@ -111,21 +102,21 @@ router.get('/search', (req, res) => {
                 }
             }, // AND
             // results contains bookingDateValue OR bookingDateValue = ''
-            bookingDate: { 
+            bookingDate: {
                 [Op.or]: {
                     [Op.like]: '%' + bookingDateValue + '%',
                     [Op.eq]: ''
                 }
             }, // AND
             // results contains paymentMethodValue OR paymentMethodValue = ''
-            paymentMethod: { 
+            paymentMethod: {
                 [Op.or]: {
                     [Op.like]: '%' + paymentMethodValue + '%',
                     [Op.eq]: ''
                 }
             }, // AND
             // paymentAmount <= paymentAmountValue OR paymentAmount < 5000
-            paymentAmount: { 
+            paymentAmount: {
                 [Op.or]: {
                     [Op.eq]: paymentAmountValue,
                     [Op.lt]: 5000
@@ -133,8 +124,9 @@ router.get('/search', (req, res) => {
             },
         }
     })
-    .then(booking => res.render('booking', { booking }))
-    .catch(err => console.log(err));
+        .then(booking => res.render('booking', { booking }))
+        .catch(err => console.log(err));
 });
 
+// Export router
 module.exports = router;
--- routes/flight.js
+++ routes/flight.js
@@ -5,8 +5,9 @@ const Flight = require('../models/Flight');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
-// Find all the rows in the flight table 
-router.get('/', (req, res) => 
+
+/******************************************** Display all rows in the flight table ********************************/
+router.get('/', (req, res) =>
     Flight.findAll()
         .then(flight => {
             res.render('flight', {
@@ -15,13 +16,14 @@ router.get('/', (req, res) =>
         })
         .catch(err => console.log(err)));
 
-// Test adding a row
+
+/********************************************** Test adding a flight **********************************************/
 router.get('/add', (req, res) => {
     const data = {
         airline: 'Boring JS Airline',
         flightDate: 'Mar-30-2019',
         startLocation: 'Vancouver',
-        endLocation: 'Italy',
+        endLocation: 'Los Angeles',
         scheduledLeavingTime: '12:00',
         estimatedArrivalTime: '23:00'
     }
@@ -42,34 +44,70 @@ router.get('/add', (req, res) => {
         .catch(err => console.log(err));
 });
 
-// Search for flights
+/********************************************** Search flights ****************************************************/
 router.get('/search', (req, res) => {
-    let { airlineValue } = req.query || '';
-    let { flightDateValue } = req.query || '';
-    let { startLocationValue } = req.query || '';
-    let { endLocationValue } = req.query || '';
-    let { scheduledLeavingTimeValue } = req.query || '';
-    let { estimatedArrivalTimeValue } = req.query || '';
+    let { airlineValue } = req.query;
+    let { flightDateValue } = req.query;
+    let { startLocationValue } = req.query;
+    let { endLocationValue } = req.query;
+    let { scheduledLeavingTimeValue } = req.query;
+    let { estimatedArrivalTimeValue } = req.query;
     // make lowercase
-    // airlineValue = airlineValue.toLowerCase();
-    // flightDateValue = flightDateValue.toLowerCase();
-    // startLocationValue = startLocationValue.toLowerCase();
-    // endLocationValue = endLocationValue.toLowerCase();
-    // scheduledLeavingTimeValue = scheduledLeavingTimeValue.toLowerCase();
-    // estimatedArrivalTimeValue = estimatedArrivalTimeValue.toLowerCase();
+    airlineValue = airlineValue.toLowerCase();
+    flightDateValue = flightDateValue.toLowerCase();
+    startLocationValue = startLocationValue.toLowerCase();
+    endLocationValue = endLocationValue.toLowerCase();
+    scheduledLeavingTimeValue = scheduledLeavingTimeValue.toLowerCase();
+    estimatedArrivalTimeValue = estimatedArrivalTimeValue.toLowerCase();
 
     Flight.findAll({
-            where: {
-                airline: {[Op.like]: '%' + airlineValue + '%'},
-                flightDate: {[Op.like]: '%' + flightDateValue + '%'},
-                startLocation: {[Op.like]: '%' + startLocationValue + '%'},
-                endLocation: {[Op.like]: '%' + endLocationValue + '%'},
-                scheduledLeavingTime: {[Op.like]: '%' + scheduledLeavingTimeValue + '%'},
-                estimatedArrivalTime: { [Op.like]: '%' + estimatedArrivalTimeValue +'%' }
+        where: {
+            // results contains airlineValue OR ''
+            airline: {
+                [Op.or]: {
+                    [Op.like]: '%' + airlineValue + '%',
+                    [Op.eq]: ''
+                }
+            }, // AND
+            // results contains flightDateValue OR ''
+            flightDate: {
+                [Op.or]: {
+                    [Op.like]: '%' + flightDateValue + '%',
+                    [Op.eq]: ''
+                }
+            }, // AND
+            // results contains startLocationValue OR ''
+            startLocation: {
+                [Op.or]: {
+                    [Op.like]: '%' + startLocationValue + '%',
+                    [Op.eq]: ''
+                }
+            }, // AND
+            // results contains endLocationValue OR ''
+            endLocation: {
+                [Op.or]: {
+                    [Op.like]: '%' + endLocationValue + '%',
+                    [Op.eq]: ''
+                }
+            }, // AND
+            // results contains scheduledLeavingTimeValue OR ''
+            scheduledLeavingTime: {
+                [Op.or]: {
+                    [Op.like]: '%' + scheduledLeavingTimeValue + '%',
+                    [Op.eq]: ''
+                }
+            }, // AND
+            // results contains estimatedArrivalTimeValue OR ''
+            estimatedArrivalTime: {
+                [Op.or]: {
+                    [Op.like]: '%' + estimatedArrivalTimeValue + '%',
+                    [Op.eq]: ''
+                }
             }
-        })
+        } // End of Where
+    }) // End of FindAll
         .then(flight => res.render('flight', { flight }))
         .catch(err => console.log(err));
-});
+}); // End of router.get('/search', (req, res)
 
 module.exports = router;
