17ae81f3cf1c355355ade171723c93137edc2a7d
patrickjiang <jzc1828@gmail.com>
2019-04-04 17:51:56 -0700

    Added comments for booking and customer routes

--- routes/booking.js
+++ routes/booking.js
@@ -1,11 +1,22 @@
+/* eslint-disable no-console */
+
+// Use express dependency
 const express = require('express');
+// Creates a route for the booking table
 const router = express.Router()
-const db = require('../config/database');
+
+// Use the Booking Model
 const Booking = require('../models/Booking');
+
+// Use sequelize dependency
 const Sequelize = require('sequelize');
+// Use sequelize operators
 const Op = Sequelize.Op;
 
+
 /******************************************* Display all rows in the booking table ********************************/
+
+// Url: /booking
 router.get('/', (req, res) =>
     Booking.findAll()
         .then(booking => res.render('booking', {
@@ -15,23 +26,31 @@ router.get('/', (req, res) =>
 
 
 /********************************************** Add a new booking *************************************************/
-// Display form to add a new booking
+
+// Display the form to add a new booking
 router.get('/add-booking', (req, res) => res.render('add-booking'));
 
 // Add a booking
 router.post('/add-booking', (req, res) => {
+
     // destructure the data object
-    let { bookingDate, paymentAmount, paymentMethod, flightId, customerId, userId } = req.body;
+    let { bookingDate,
+        paymentAmount,
+        paymentMethod,
+        flightId,
+        customerId,
+        userId } = req.body;
     let errors = [];
 
+    // Error handling
     if (!bookingDate) {
         errors.push({ text: 'Please add booking date' })
     }
-    if (!paymentAmount) {
-        errors.push({ text: 'Please add payment amount' })
-    }
     if (!paymentMethod) {
         errors.push({ text: 'Please add a payment method' })
+    }
+    if (!paymentAmount) {
+        errors.push({ text: 'Please add payment amount' })
     }
     if (!flightId) {
         errors.push({ text: 'Please add a flight id' })
@@ -43,7 +62,7 @@ router.post('/add-booking', (req, res) => {
         errors.push({ text: 'Please add a user id' })
     }
 
-    // Check for errors
+    // Display erros if there is any
     if (errors.length > 0) {
         res.render('add-booking', {
             errors,
@@ -54,42 +73,54 @@ router.post('/add-booking', (req, res) => {
             customerId,
             userId
         });
-    } else {
+    } else { // No errors, then create a new booking
+
+        // if there is no payment amount entered, set default to 0
         if (!paymentAmount) {
             paymentAmount = 0;
         } else {
             paymentAmount = `${paymentAmount}`;
         }
 
-        // Insert data into the flight table
+        // Insert data into the booking table
         Booking.create({
             bookingDate,
-            paymentAmount,
             paymentMethod,
+            paymentAmount,
             flightId,
             customerId,
             userId
         })
-            .then(booking => res.redirect('/booking'))
+            // redirect back to the booking page showing all the rows if succeed
+            .then(() => res.redirect('/booking'))
+            // show errors if adding not succeed
             .catch(err => console.log(err));
     }
 });
 
 /********************************************** Search bookings ****************************************************/
+
+/* Search based on:
+    1. booking id only 
+    2. payment amount only (search by all paymentAmount <= searched amount)
+    3. booking date AND payment method
+    4. all attributes together
+*/
 router.get('/search', (req, res) => {
-    // destructure querry object
+
+    // Destructure querry object
     let { bookingIdValue } = req.query;
     let { bookingDateValue } = req.query;
     let { paymentMethodValue } = req.query;
     let { paymentAmountValue } = req.query;
 
-    // Make lowercase
+    // Make them Lowercase
     bookingIdValue = bookingIdValue.toLowerCase();
     bookingDateValue = bookingDateValue.toLowerCase();
     paymentMethodValue = paymentMethodValue.toLowerCase();
     paymentAmountValue = paymentAmountValue.toLowerCase();
 
-    // Search unique bookingId
+    /* Search by unique bookingId */
     if (bookingIdValue != '') {
         Booking.findAll({
             where: {
@@ -97,16 +128,18 @@ router.get('/search', (req, res) => {
                 bookingId: { [Op.eq]: bookingIdValue }
             }
         })
+            // Display the search results if succeed
             .then(booking => res.render('booking', { booking }))
+            // Show errors if not succeed
             .catch(err => console.log(err));
 
-        // Search all specified payment amount
+        /* Search by specified payment amount */
     } else if (paymentAmountValue != '') {
         Booking.findAll({
             where: {
-                // bookingId = [0, 100]
+                // bookingId = [1, 1000]
                 bookingId: {
-                    [Op.between]: [1, 100]
+                    [Op.between]: [1, 1000]
                 }, // AND
                 // Results contains bookingDateValue OR bookingDateValue = ''
                 bookingDate: {
@@ -129,11 +162,12 @@ router.get('/search', (req, res) => {
                     },
                 }
             } // End of Where
+
         }) // End of FindAll
             .then(booking => res.render('booking', { booking }))
             .catch(err => console.log(err));
 
-        // search specified booking date OR payment method
+        /* search by specified booking date AND payment method */
     } else if (paymentAmountValue == '') {
         Booking.findAll({
             where: {
@@ -158,30 +192,40 @@ router.get('/search', (req, res) => {
                 // paymentAmount <= paymentAmountValue OR paymentAmount < 5000
                 paymentAmount: {
                     [Op.or]: {
-                        [Op.lte]: 5000
+                        [Op.lte]: 10000
                     },
                 }
+
             } // End of Where
+
         }) // End of FindAll
+            // Display the search results if succeed
             .then(booking => res.render('booking', { booking }))
+            // Show errors if not succeed
             .catch(err => console.log(err));
-    }
+
+    } // End of else if
 
 }); // End of router.get('/search', (req, res)
 
+
 /************************************************* Edit booking *************************************************/
-// Display form to edit a user
+
+// Display the form to edit a user
 router.get('/edit-booking', (req, res) => res.render('edit-booking'));
 
 // Edit a user
 router.post('/edit-booking', (req, res) => {
     // destructure the data object
-    let { existingBookingId, newBookingDate, newPaymentMethod, newPaymentAmount } = req.body;
+    let { existingBookingId,
+        newBookingDate,
+        newPaymentMethod,
+        newPaymentAmount } = req.body;
     let errors = [];
 
     // Error handling
     if (!existingBookingId) {
-        errors.push({ text: 'Please add a new booking id' })
+        errors.push({ text: 'Please add an existing booking Id' })
     }
     if (!newBookingDate) {
         errors.push({ text: 'Please add a new booking date' })
@@ -193,7 +237,7 @@ router.post('/edit-booking', (req, res) => {
         errors.push({ text: 'Please add a new payment amount' })
     }
 
-    // Check for errors
+    // Display erros if there is any
     if (errors.length > 0) {
         res.render('edit-booking', {
             errors,
@@ -202,8 +246,8 @@ router.post('/edit-booking', (req, res) => {
             newPaymentMethod,
             newPaymentAmount
         });
-    } else {
-        // Make data lowercase
+    } else { // No errors, then edit a booking
+        // Make Lowercase
         existingBookingId = existingBookingId.toLowerCase();
         newBookingDate = newBookingDate.toLowerCase();
         newPaymentMethod = newPaymentMethod.toLowerCase();
@@ -214,39 +258,41 @@ router.post('/edit-booking', (req, res) => {
             bookingDate: newBookingDate,
             paymentMethod: newPaymentMethod,
             paymentAmount: newPaymentAmount
-        },
-            {
-                // Where clause
-                where: {
+        }, {
+                where: { // where bookingId = existingBookingId 
                     bookingId: existingBookingId
                 }
             }
         )
-            .then(booking => res.redirect('/booking'))
+            // Redirect to the booking page if succeed
+            .then(() => res.redirect('/booking'))
+            // Show errors if not succeed
             .catch(err => console.log(err));
     } // End of else
 });
 
+
 /************************************************* Delete booking *************************************************/
+
+// Display the delete form
 router.get('/delete-booking', (req, res) => res.render('delete-booking'));
 
+// Delete a booking
 router.post('/delete-booking', (req, res) => {
+
+    // Destructure the object
     let { bookingIddelete } = req.body;
+
+    // Delete a booking
     Booking.destroy({
+        // Where: bookingId = bokkingIdDelete
         where: { bookingId: bookingIddelete }
-    }).then(booking => res.redirect('/booking'))
+    })
+        // Redirect to the booking page if succeed
+        .then(() => res.redirect('/booking'))
+        // Show errors if not succeed
         .catch(err => console.log(err));
 });
 
-/* const Handlebars = require('handlebars');
-Handlebars.registerHelper('deletebooking', function (id) {
-    Booking.destroy({
-        where: {bookingId :id }
-    }).then(booking => res.redirect('/booking'))
-    .catch(err => console.log(err));
-});*/
-
-
-
-// Export router
+// Export the router
 module.exports = router;
--- routes/customer.js
+++ routes/customer.js
@@ -1,11 +1,21 @@
+/* eslint-disable no-console */
+
+// Use express dpendency
 const express = require('express');
+// Creates a route for the booking table
 const router = express.Router()
-const db = require('../config/database');
+
+// Use the Customer Model
 const Customer = require('../models/Customer');
+
+// Use sequelize dependency
 const Sequelize = require('sequelize');
+// Use sequelize operators
 const Op = Sequelize.Op;
 
+
 /****************************************** Display all rows in the customer table ********************************/
+// Url: /customer
 router.get('/', (req, res) =>
     Customer.findAll()
         .then(customer => {
@@ -15,33 +25,40 @@ router.get('/', (req, res) =>
         })
         .catch(err => console.log(err)));
 
+
 /********************************************* Add a new customer *************************************************/
+
 // Display form to add a new customer
 router.get('/add-customer', (req, res) => res.render('add-customer'));
 
 // Add a customer
 router.post('/add-customer', (req, res) => {
     // destructure the data object
-    let { firstName, lastName, email, telephone, address } = req.body;
+    let { firstName,
+        lastName,
+        email,
+        telephone,
+        address } = req.body;
     let errors = [];
 
+    // Error handling
     if (!firstName) {
-        errors.push({ text: 'Please add a first name' })
+        errors.push({ text: 'Please add first name' })
     }
     if (!lastName) {
-        errors.push({ text: 'Please add a last name' })
+        errors.push({ text: 'Please add last name' })
     }
     if (!email) {
-        errors.push({ text: 'Please add an email' })
+        errors.push({ text: 'Please add email' })
     }
     if (!telephone) {
-        errors.push({ text: 'Please add a telephone' })
+        errors.push({ text: 'Please add telephone' })
     }
     if (!address) {
-        errors.push({ text: 'Please add an address' })
+        errors.push({ text: 'Please add address' })
     }
 
-    // Check for errors
+    // Display erros if there is any
     if (errors.length > 0) {
         res.render('add-customer', {
             errors,
@@ -51,7 +68,7 @@ router.post('/add-customer', (req, res) => {
             telephone,
             address
         });
-    } else {
+    } else { // No errors, then create a new customer
 
         // Make data lowercase
         firstName = firstName.toLowerCase();
@@ -68,14 +85,23 @@ router.post('/add-customer', (req, res) => {
             telephone,
             address
         })
-            .then(customer => res.redirect('/customer'))
+            // redirect back to the customer page showing all the rows if succeed
+            .then(() => res.redirect('/customer'))
+            // show errors if adding not succeed
             .catch(err => console.log(err));
     }
 });
 
 
 /********************************************** Search customers **************************************************/
+/* Search based on:
+    1. booking id only 
+    2. payment amount only (search by all paymentAmount <= searched amount)
+    3. booking date AND payment method
+    4. all attributes together
+*/
 router.get('/search', (req, res) => {
+
     // Destructure querry object
     let { customerIdValue } = req.query;
     let { firstNameValue } = req.query;
@@ -84,7 +110,7 @@ router.get('/search', (req, res) => {
     let { telephoneValue } = req.query;
     let { addressValue } = req.query;
 
-    // Make lowercase
+    // Make Lowercase
     customerIdValue = customerIdValue.toLowerCase();
     firstNameValue = firstNameValue.toLowerCase();
     lastNameValue = lastNameValue.toLowerCase();
@@ -92,55 +118,67 @@ router.get('/search', (req, res) => {
     telephoneValue = telephoneValue.toLowerCase();
     addressValue = addressValue.toLowerCase();
 
-    Customer.findAll({
-        where: {
-            // customerId = customerIdValue
-            customerId: {
-                [Op.or]: {
+    /* Search by unique customerId */
+    if (customerId != '') {
+        Customer.findAll({
+            where: {
+                // customerId = customerIdValue
+                customerId: {
                     [Op.eq]: customerIdValue,
                     [Op.between]: [0, 100]
                 }
-            }, // AND
-            // Results contains firstNameValue OR firstNameValue = ''
-            firstName: {
-                [Op.or]: {
-                    [Op.like]: '%' + firstNameValue + '%',
-                    [Op.eq]: ''
-                }
-            }, // AND
-            // Results contains lastNameValue OR lastNameValue = ''
-            lastName: {
-                [Op.or]: {
-                    [Op.like]: '%' + lastNameValue + '%',
-                    [Op.eq]: ''
-                }
-            }, // AND
-            // Results contains emailValue OR emailValue = ''
-            email: {
-                [Op.or]: {
-                    [Op.like]: '%' + emailValue + '%',
-                    [Op.eq]: ''
-                }
-            }, // AND
-            // Results contains emailValue OR emailValue = ''
-            telephone: {
-                [Op.or]: {
-                    [Op.like]: '%' + telephoneValue + '%',
-                    [Op.eq]: ''
-                }
-            }, // AND 
-            // Results contains addressValue OR addressValue = ''
-            address: {
-                [Op.or]: {
-                    [Op.like]: '%' + addressValue + '%',
-                    [Op.eq]: ''
-                }
             }
-        } // End of Where
-    }) // End of FindAll
-        .then(customer => res.render('customer', { customer }))
-        .catch(err => console.log(err));
-}); // End of router.get('/search', (req, res)
+        })
+            .then(customer => res.render('customer', { customer }))
+            .catch(err => console.log(err));
+
+    } else {
+        Customer.findAll({
+            where: {
+                // customerId = [1, 1000]
+                customerId: {
+                    [Op.between]: [1, 1000]
+                }, // AND
+                // Results contains firstNameValue OR firstNameValue = ''
+                firstName: {
+                    [Op.or]: {
+                        [Op.like]: '%' + firstNameValue + '%',
+                        [Op.eq]: ''
+                    }
+                }, // AND
+                // Results contains lastNameValue OR lastNameValue = ''
+                lastName: {
+                    [Op.or]: {
+                        [Op.like]: '%' + lastNameValue + '%',
+                        [Op.eq]: ''
+                    }
+                }, // AND
+                // Results contains emailValue OR emailValue = ''
+                email: {
+                    [Op.or]: {
+                        [Op.like]: '%' + emailValue + '%',
+                        [Op.eq]: ''
+                    }
+                }, // AND
+                // Results contains emailValue OR emailValue = ''
+                telephone: {
+                    [Op.or]: {
+                        [Op.like]: '%' + telephoneValue + '%',
+                        [Op.eq]: ''
+                    }
+                }, // AND 
+                // Results contains addressValue OR addressValue = ''
+                address: {
+                    [Op.or]: {
+                        [Op.like]: '%' + addressValue + '%',
+                        [Op.eq]: ''
+                    }
+                }
+            } // End of Where
+        }) // End of FindAll
+            .then(customer => res.render('customer', { customer }))
+            .catch(err => console.log(err));
+    }); // End of router.get('/search', (req, res)
 
 /************************************************* Edit booking *************************************************/
 // Display form to edit a user
